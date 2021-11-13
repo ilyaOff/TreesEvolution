9651fe5f230806fcf4d2dc84815fba93e613e815
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2021-11-13 22:47:40 +0300

    Update Tree
    Изменена функция добавления узла. Теперь каждый новый узел берет следующее по ДНК правило узла

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    private static int countRules = 5;
+    private static int countRules = 6;
     private static int lengthDNA = 50;
     public int timeToLive = 30;
     public int maxTlmeToLive = 100;
 
     public Transform prefNode;
+    [SerializeField]
     private int[] DNA;
     [SerializeField]
     List<Node> bodyNodes;
@@ -21,12 +22,13 @@ public class Tree : MonoBehaviour
     {
         DNA = new int[lengthDNA];
         for (int i = 0; i < lengthDNA; i++)
-            DNA[i] = Random.Range(0, countRules*2);
+            DNA[i] = Random.Range(0, countRules);
 
         bodyNodes = new List<Node>();
         bodyNodes.Add(Instantiate(prefNode, transform.position, Quaternion.identity, this.transform).gameObject.GetComponent<Node>());
         branchNodes = new List<Node>();
         branchNodes.Add(bodyNodes[0]);
+        bodyNodes[0].rules = 0;
     }
 
     private void Update()
@@ -41,37 +43,51 @@ public class Tree : MonoBehaviour
     void Tick()
     {
         timeToLive--;
-        if (timeToLive <= 0) return;
+        if (timeToLive <= 0) Destroy(this.transform.gameObject);
         //int length = branchNodes.Count;
+        List<Node> tmpBranchNodes = new List<Node>();
         foreach (Node node in branchNodes)
         { 
-            switch(node.rules)
+            switch(DNA[node.rules])
             {                
                 case 0://UP
-                    AddNode(Vector3.up, node.transform, 0);
+                    tmpBranchNodes.Add( AddNode(Vector3.up, node));
                     break;
                 case 1://Forvard
-                    AddNode(Vector3.forward, node.transform, 1);
+                    tmpBranchNodes.Add(AddNode(Vector3.forward, node));
                     break; 
                 case 2://Rigth
-                    AddNode(Vector3.right, node.transform, 2);
+                    tmpBranchNodes.Add(AddNode(Vector3.right, node));
                     break;
                 case 3://Back
-                    AddNode(Vector3.back, node.transform, 3);
+                    tmpBranchNodes.Add(AddNode(Vector3.back, node));
                     break;
                 case 4://Left
-                    AddNode(Vector3.left, node.transform, 4);
+                    tmpBranchNodes.Add(AddNode(Vector3.left, node));
+                    break;
+                case 5: //Full branch
+                    tmpBranchNodes.Add(AddNode(Vector3.up, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.forward, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.right, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.back, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.left, node));
                     break;
                 default: break;
             }
-            branchNodes.Remove(node);
+            //branchNodes.Remove(node);
         }
+        branchNodes.Clear();
+        branchNodes = tmpBranchNodes;
     }
-    void AddNode(Vector3 pos, Transform parent, int parentRules)
+    Node AddNode(Vector3 pos, Node parent)
     {
-        Node newNode = Instantiate(prefNode, parent.position + pos, Quaternion.identity, parent).gameObject.GetComponent<Node>();
-        newNode.rules = (parentRules + 1) % (2 * countRules);
+        Node newNode = Instantiate(prefNode, parent.transform.position + pos, Quaternion.identity, parent.transform)
+                        .gameObject.GetComponent<Node>();
+       
+        print((parent.rules).ToString() + " " +((parent.rules + 1) % (lengthDNA)).ToString());
+        newNode.rules = (parent.rules + 1) % (lengthDNA);
         bodyNodes.Add(newNode);
-        branchNodes.Add(newNode);
+        return newNode;
+        //branchNodes.Add(newNode);
     }
 }
