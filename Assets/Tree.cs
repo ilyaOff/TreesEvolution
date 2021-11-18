1659e43e269c52070f5f2c621216f5bf0662f378
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2021-11-18 17:27:35 +0300

    Check Instantiate
    Добавлена проверка, можно ли установить узел в текущую позицию. Для разветвления сделан поворот всех узлов, но это мешает работе проверки, можно ли установить узел

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -50,47 +50,59 @@ public class Tree : MonoBehaviour
         //int length = branchNodes.Count;
         List<Node> tmpBranchNodes = new List<Node>();
         foreach (Node node in branchNodes)
-        { 
-            switch(DNA[node.rules])
+        {
+           
+            switch (DNA[node.rules])
             {                
                 case 0://UP
-                    tmpBranchNodes.Add( AddNode(Vector3.up, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.up, node, node.transform.rotation));
                     break;
                 case 1://Forvard
-                    tmpBranchNodes.Add(AddNode(Vector3.forward, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.forward, node, node.transform.rotation));
                     break; 
                 case 2://Rigth
-                    tmpBranchNodes.Add(AddNode(Vector3.right, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.right, node, node.transform.rotation));
                     break;
                 case 3://Back
-                    tmpBranchNodes.Add(AddNode(Vector3.back, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.back, node, node.transform.rotation));
                     break;
                 case 4://Left
-                    tmpBranchNodes.Add(AddNode(Vector3.left, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.left, node, node.transform.rotation));
                     break;
                 case 5: //Full branch
-                    tmpBranchNodes.Add(AddNode(Vector3.up, node));
-                    tmpBranchNodes.Add(AddNode(Vector3.forward, node));
-                    tmpBranchNodes.Add(AddNode(Vector3.right, node));
-                    tmpBranchNodes.Add(AddNode(Vector3.back, node));
-                    tmpBranchNodes.Add(AddNode(Vector3.left, node));
+                    tmpBranchNodes.Add(AddNode(Vector3.up, node, node.transform.rotation));
+                    tmpBranchNodes.Add(AddNode(Vector3.forward, node, node.transform.rotation));
+                    tmpBranchNodes.Add(AddNode(Vector3.right, node, node.transform.rotation * Quaternion.AngleAxis(90, transform.up)));
+                    tmpBranchNodes.Add(AddNode(Vector3.back, node, node.transform.rotation * Quaternion.AngleAxis(180, transform.up)));
+                    tmpBranchNodes.Add(AddNode(Vector3.left, node, node.transform.rotation * Quaternion.AngleAxis(-90, transform.up)));
                     break;
                 default: break;
             }
             //branchNodes.Remove(node);
         }
+        //удалить все null узлы
+        tmpBranchNodes.RemoveAll(item => item == null);
+
         branchNodes.Clear();
         branchNodes = tmpBranchNodes;
     }
-    Node AddNode(Vector3 pos, Node parent)
+    Node AddNode(Vector3 pos, Node parent, Quaternion rot)
     {
-        Node newNode = Instantiate(prefNode, parent.transform.position + pos, Quaternion.identity, parent.transform)
+        
+        if (!CheckInstantiate(parent.transform.position, (parent.transform.localPosition + pos)))//ошибка при повороте
+            return null;
+        
+       Node newNode = Instantiate(prefNode, Vector3.zero, rot, parent.transform)
                         .gameObject.GetComponent<Node>();
-       
+        newNode.transform.localPosition = pos;
         print((parent.rules).ToString() + " " +((parent.rules + 1) % (lengthDNA)).ToString());
         newNode.rules = (parent.rules + 1) % (lengthDNA);
         bodyNodes.Add(newNode);
         return newNode;
         //branchNodes.Add(newNode);
+    }
+    bool CheckInstantiate(Vector3 start, Vector3 dir)
+    {      
+        return !Physics.Raycast(start, dir, 2f);
     }
 }
